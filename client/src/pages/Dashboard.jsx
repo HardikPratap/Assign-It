879d1fa6babe2c310a7dcc1f239eae3dc4ce9519
Hardik Pratap <hardikpratap188@gmail.com>
2024-12-03 00:52:42 +0530

    created top10 task list

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -11,6 +11,78 @@ import { FaNewspaper, FaUsers } from "react-icons/fa";
 import { FaArrowsToDot } from "react-icons/fa6";
 import moment from "moment";   // whats moment used for?
 import clsx from 'clsx';
+import Charts from '../components/Charts';
+import { BGS, PRIOTITYSTYELS, TASK_TYPE } from '../utils';
+import UserInfo from '../components/UserInfo';
+
+function TaskTable({tasks}) {
+  const ICONS = {
+    high: <MdKeyboardDoubleArrowUp />,
+    medium: <MdKeyboardArrowUp />,
+    low: <MdKeyboardArrowDown />,
+  };
+
+  const TableHeader= ()=>(
+    <thead className='border-b border-gray-300 '>
+      <tr className='text-white text-left'>
+        <th className='py-2'>Task Title</th>
+        <th className='py-2'>Priority</th>
+        <th className='py-2'>Team</th>
+        <th className='py-2 hidden md:block'>Created At</th>
+      </tr>
+    </thead>
+  )
+
+  const TableRow= ({task})=>(
+    <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10 '>
+      <td className='py-2'>
+        <div className='flex items-center gap-2'>
+          <div  className= {clsx("w-4 h-4 rounded-full" ,TASK_TYPE[task.stage])}/>
+          <p className='text-base text-black'>{task.title}</p>
+        </div>
+      </td>
+
+      <td className='py-2'>
+        <div className='flex gap-1 items-center'>
+          <span className={clsx("text-lg", PRIOTITYSTYELS[task.priority])}>
+            {ICONS[task.priority]}
+          </span>
+          <span className='capitalize text-gray-500'>{task.priority}</span>
+        </div>
+      </td>
+
+      <td className='py-2'>
+        <div className='flex'>
+          {task.team.map((m, index) => (
+            <div
+              key={index}
+              className={clsx(
+                "w-7 h-7 rounded-full text-white flex items-center justify-center text-sm -mr-1",
+                BGS[index % BGS.length]
+              )}
+            >
+              <UserInfo user={m} />
+            </div>
+          ))}
+        </div>
+      </td>
+    </tr>
+
+  )
+
+  return <div className='w-full md:w-2/3 bg-gray-700 px-2 md:px-4 pt-4 pb-4 shadow-md rounded'>
+    <table>
+      <TableHeader />
+      <tbody>
+        {
+          tasks.map((task,id)=>(
+            <TableRow key={id} task={task} />
+          ))
+        }
+      </tbody>
+    </table>
+  </div>
+}
 
 function Dashboard() {
 
@@ -76,6 +148,25 @@ function Dashboard() {
           ))
         }
       </div>
+
+      <div className='w-full bg-gray-700 my-16 p-4 rounded shadow-sm'>
+        <h4 className='text-xl text-white font-semibold'>
+          Chart by Priority
+        </h4>
+        <Charts />
+      </div>
+
+      <div className='w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8'>
+        {/* /left */}
+
+        <TaskTable tasks={summary.last10Task} />
+
+        {/* /right */}
+
+        {/* <UserTable users={summary.users} /> */}
+      </div>
+
+
     </div>
   )
 }
