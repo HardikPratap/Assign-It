54c194e5b0cd7a8034c6260322a750e6d0e1c301
Hardik Pratap <hardikpratap188@gmail.com>
2024-12-16 00:59:24 +0530

    userModel done

---
+++ server/modules/User.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    title: { type: String, required: true },
+    role: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    isActive: { type: Boolean, required: true, default: true },
+    tasks: [{ type: Schema.ObjectId, ref: "Task" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.method.matchPassword = async function (enteredPass) {
+  return await bcrypt.compare(enteredPass, this.password);
+};
+
+const USerModel = mongoose.model("User", userSchema);
