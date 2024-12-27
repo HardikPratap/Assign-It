5e903b4f2506b52176d06ea146643dbae93e8299
Hardik Pratap <hardikpratap188@gmail.com>
2024-12-27 23:04:39 +0530

    update login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -83,7 +83,7 @@ function Login() {
             </form>
 
                 <div className="inline-flex items-center justify-center w-full">
-                    <hr className="w-1/2 h-1 my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
+                    <hr className="w-1/2 h-1 my-8 dark:bg-gray-200 border-2 bg-gray-700"/>
                     <span className="absolute px-3 font-medium bg-transparent justify-center text-white dark:bg-black">Login <span className='text-xs text-gray-400'>with Others</span></span>
                 </div>
         </div>
