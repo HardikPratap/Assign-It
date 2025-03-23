6e13ebadc60c6f5d89e294a031a85fbf05c87d25
Hardik Pratap <hardikpratap188@gmail.com>
2025-03-24 01:05:42 +0530

    icon update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import {
   MdKeyboardDoubleArrowUp,
 } from "react-icons/md";
 import { summary, user } from "../assets/data";
-import { LuClipboardPen } from "react-icons/lu";
+import { LuClipboardEdit } from "react-icons/lu";
 import { FaNewspaper, FaUsers } from "react-icons/fa";
 import { FaArrowsToDot } from "react-icons/fa6";
 import moment from "moment";   // whats moment used for?
@@ -98,7 +98,7 @@ function Dashboard() {
       _id: "3",
       label: "TASK IN PROGRESS ",
       total: totals["in progress"] || 0,
-      icon: <LuClipboardPen />,
+      icon: <LuClipboardEdit />,
       bg: "bg-[#f59e0b]",
     },
     {
