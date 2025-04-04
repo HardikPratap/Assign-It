d8f2fb2ff5ea8eaecee7a26c4fd459d6a1ca4e62
Hardik Pratap <hardikpratap188@gmail.com>
2025-04-04 11:35:20 +0530

    changed the port and api route

--- client/src/redux/splice/apiSlice.js
+++ client/src/redux/splice/apiSlice.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const API_URL = "http://localhost:8000/api/v1";
+// const API_URL = "http://localhost:8000/api";
 const API_URL = import.meta.env.VITE_APP_BASE_URL;
 
-const baseQuery = fetchBaseQuery({ baseUrl: API_URL + "/api/v1" });
+const baseQuery = fetchBaseQuery({ baseUrl: API_URL + "/api" });
 
 const apiSlice = createApi({
   baseQuery,
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:5000", "http://localhost:5001"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
