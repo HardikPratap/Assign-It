1da466ff9c8fec7a945d7b847546794b246ad17d
Hardik Pratap <hardikpratap188@gmail.com>
2025-05-01 01:13:08 +0530

    final commit

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
 import Dashboard from "./Dashboard";
 import Login from "./Login";
-import TaskDetail from "./TaskDetail";
 import Tasks from "./Tasks";
 import Trash from "./Trash";
+import TaskDetail from "./TaskDetail";
 import Users from "./Users";
 import StatusPage from "./Status";
 
